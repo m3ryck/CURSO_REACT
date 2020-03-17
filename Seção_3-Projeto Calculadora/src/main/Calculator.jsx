eb22ff8d1bceb2b86d758bb0b810ba433f159f6a
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2020-03-17 10:04:05 -0300

    Aula 39

--- Seção_3-Projeto Calculadora/src/main/Calculator.jsx
+++ Seção_3-Projeto Calculadora/src/main/Calculator.jsx
@@ -21,6 +21,56 @@ export default class Caculator extends Component{
         this.setState({...initialState})
     }
     setOperation(operation){
+        if(this.state.current === 0){
+            this.setState({operation,current:1,clearDisplay: true})
+        //se chegar aqui, uma operação ja foi setada
+        }else{
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+
+            /**************** */
+            switch(currentOperation) {
+                case "/":
+                    values[0]=values[0]/values[1]
+                    values[1]=0
+                    break 
+                case "+":
+                    values[0]=values[0]+values[1]
+                    values[1]=0
+                    break 
+                case "-":
+                    values[0]=values[0]-values[1]
+                    values[1]=0
+                    break 
+                case "*":
+                    values[0]=values[0]*values[1]
+                    values[1]=0
+                    break 
+        
+                default: values[0] = this.state.values[0]
+            }
+
+
+            /**************** */
+
+            // try{
+            //     values[0]=eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            // }catch(e){
+            //     values[0] = this.state.values[0]
+            // }
+
+            //values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
         console.log(operation)
     }
     addDigit(n){
@@ -32,7 +82,7 @@ export default class Caculator extends Component{
         //limpa o display caso o estado atual seja apenas 0, ou se o estado de clearDisplay for True
         const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
 
-        //
+        //salva o valor do display em currentValue , e adiciona novos digitos
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
         this.setState({displayValue,clearDisplay: false})
@@ -44,8 +94,6 @@ export default class Caculator extends Component{
             values[i] = newValue
             this.setState({values})
         }
-
-
 
     }
 
