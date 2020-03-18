6178dd8735fae9b73407e973af0f67013078a93a
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2020-03-18 17:08:06 -0300

    Aula - 50

---
+++ Seção_4-5-Projeto TodoApp(backend)/backend/src/api/todo/todo.js
@@ -0,0 +1,12 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+//tarefa que tem como descrição uma string, 
+//e um atributo boolean, que informa se concluiu ou nao, por default é falso
+const todoSchema = new mongoose.Schema({
+    description: {type: String, required: true},
+    done: {type:Boolean,required: true, default:false},
+    createdAt:{type: Date, default: Date.now}
+})
+
+module.exports = restful.model('Todo',todoSchema)
---
+++ Seção_4-5-Projeto TodoApp(backend)/backend/src/loader.js
@@ -0,0 +1,5 @@
+//carrega os principais arquivos de configuração
+const server = require('./config/server')
+require('./config/database')
+require('./config/routes')(server)
+
