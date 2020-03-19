1035e2cd30ec7a23b47b1028ac259b1f59c8040a
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2020-03-19 15:55:25 -0300

    Aula 57 - Componentes todoForm & todoList

--- Seção_4-5-Projeto TodoApp(backend)/frontend/src/todo/todo.jsx
+++ Seção_4-5-Projeto TodoApp(backend)/frontend/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import PageHeader from '../template/pageHeader'
+import todoForm from './todoForm'
+import todoList from './todoList'
 
 export default class Todo extends React.Component{
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small=' cadastro'></PageHeader>
+                <todoForm/>
+                <todoList/>
             </div>
         )
     }
