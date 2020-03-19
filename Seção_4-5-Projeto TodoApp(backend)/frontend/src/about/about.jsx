9a7071496cea82b7b8c1c4f711dde6cb70304dd9
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2020-03-19 15:49:58 -0300

    Aula 56 - Componente PageHeader

--- Seção_4-5-Projeto TodoApp(backend)/frontend/src/about/about.jsx
+++ Seção_4-5-Projeto TodoApp(backend)/frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
      <div>
-         <h1>Sobre</h1>
+         <PageHeader name='Sobre' small=' Nós'></PageHeader>
+         <h2>Nossa História</h2>
+         <p>A vantagem de ter péssima memória é divertir-se muitas vezes com as mesmas coisas boas como se fosse a primeira vez</p>
+         <h2>Missão e Visão</h2>
+         <p>A vantagem de ter péssima memória é divertir-se muitas vezes com as mesmas coisas boas como se fosse a primeira vez</p>
+         <h2>Imprensa</h2>
+         <p>A vantagem de ter péssima memória é divertir-se muitas vezes com as mesmas coisas boas como se fosse a primeira vez</p>
+
      </div>
 )
