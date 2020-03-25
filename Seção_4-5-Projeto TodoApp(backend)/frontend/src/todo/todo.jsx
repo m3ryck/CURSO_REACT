502a4306501db40c7092ad9b2469b8ee33bd0e0d
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2020-03-25 17:45:23 -0300

    Aula 64 - conexão com o backend

--- Seção_4-5-Projeto TodoApp(backend)/frontend/src/todo/todo.jsx
+++ Seção_4-5-Projeto TodoApp(backend)/frontend/src/todo/todo.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
+import Axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const url = 'http://localhost:3003/api/todos'
+
 export default class Todo extends React.Component {
     constructor(props){
         super(props)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange=this.handleChange.bind(this)
+        this.state = {description: "",list:[]}
     }
     
-    handleAdd(){
-       console.log(this)  
+    handleChange(e){
+       this.setState({...this.state,description: e.target.value})
     }
+
+    handleAdd(){
+       const description = this.state.description
+       Axios.post(url,{description}).then(resp=>console.log('funcionou!'))
+     }
     render(){
         return(
         <div>
         <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-        <TodoForm handleAdd={this.handleAdd}/>
+        <TodoForm description = {this.state.description}
+        handleAdd={this.handleAdd} handleChange={this.handleChange}/>
         <TodoList/>
         </div>
         )
